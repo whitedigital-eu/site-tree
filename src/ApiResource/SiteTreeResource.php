da52babd9d0b3c90ae590f980e14d57536c845dd
Dāvis Zālītis <davis.zalitis@whitedigital.eu>
2023-06-08 11:05:29 +0300

    feat: show only active site tree items for public access

--- src/ApiResource/SiteTreeResource.php
+++ src/ApiResource/SiteTreeResource.php
@@ -18,6 +18,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use WhiteDigital\EntityResourceMapper\Attribute\Mapping;
 use WhiteDigital\EntityResourceMapper\Attribute\SkipCircularReferenceCheck;
+use WhiteDigital\EntityResourceMapper\Filters\ResourceBooleanFilter;
 use WhiteDigital\EntityResourceMapper\Filters\ResourceExistsFilter;
 use WhiteDigital\EntityResourceMapper\Filters\ResourceNumericFilter;
 use WhiteDigital\EntityResourceMapper\Filters\ResourceSearchFilter;
@@ -134,6 +135,7 @@ use WhiteDigital\SiteTree\Validator\Constraints\AllowedType;
     ),
     ApiFilter(GroupFilter::class, arguments: ['parameterName' => 'groups', 'overrideDefaultGroups' => false, ]),
     ApiFilter(ResourceExistsFilter::class, properties: ['parent', ]),
+    ApiFilter(ResourceBooleanFilter::class, properties: ['isActive', 'isVisible']),
     ApiFilter(ResourceNumericFilter::class, properties: ['level', 'parent.id', ]),
     ApiFilter(ResourceSearchFilter::class, properties: ['slug', 'slug', 'title', 'type', ]),
 ]
